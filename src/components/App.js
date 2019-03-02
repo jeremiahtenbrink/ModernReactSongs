8ade7319de793d9de1c47a485c6fc89c0ed991e8
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-03-01 21:49:57 -0700

    Init commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const App = ( props ) => {
+    return (
+        <div>App</div>
+    );
+};
+
+App.propTypes = {};
+App.defaultProps = {};
+
+export default App;
